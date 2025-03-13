18fecd67c17c9d39aebea18682746bdb2c36f01f
Svanik Bose <svanikbose@gmail.com>
2025-03-13 13:50:03 -0700

    Create headers.h

---
+++ PA 5/headers.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void print_rules();
+void start_game();
+int roll_dice ();
+int sum_dice(int dice[]);
+int sum_of_num(int dice[], int *score, int num_to_sum);
+int three_of_a_kind(int dice[], int *score, int player_combinations[13]);
+int four_of_a_kind(int dice[], int *score, int player_combinations[13]);
+int switch_player();
+int choose_combinations(int player_combinations[13]);
+int full_house(int dice[], int *score, int player_combinations[13]);
+int small_straight(int dice[], int *score, int player_combinations[13]);
+int large_straight (int dice[], int *score, int player_combinations[13]);
+int yahtzee(int dice[], int *score, int player_combinations[13]);
+int chance(int dice[], int *score, int player_combinations[13]);
