1e9e6c14556b23b28a60576f14fadafe171a8844
Svanik Bose <svanikbose@gmail.com>
2025-03-16 18:47:39 -0700

    Update functions.c

--- PA 5/functions.c
+++ PA 5/functions.c
@@ -11,6 +11,7 @@ void print_rules(){
     printf("3. These rounds will occur a total of 13 times for both players, after which the game of Yahtzee ends\n");
     printf("4. Once the sums have been totalled, if any player has obtained at least 63 points, they will be awarded a bonus of 35 points\n");
     printf("5. The player who has obtained the most amount of points wins the game!\n");
+    printf("WARNING! IF YOU CHOOSE A COMBINATION WHICH THE DICE DO NOT QUALIFY FOR, A 0 WILL BE ADDED TO THAT SCORE AND YOU CANNOT REUSE THAT COMBINATION");
     printf("____________________________________________\n");
     printf("Below are the numbers you will type for each game combination\n");
     printf("UPPER SECTION:");
@@ -24,12 +25,12 @@ void print_rules(){
     printf("LOWER SECTION\n");
     printf("____________________________________________\n");
     printf("7. Three of a Kind\n");
-    printf("8. 4 of a kind\n");
-    printf("9. Full House\n");
-    printf("10. Small Straight\n");
-    printf("11. Large Straight");
-    printf("12. Yahtzee!");
-    printf("13. Chance!");
+    printf("8. Four of a kind\n");
+    printf("9. Full House (Two of one kind, three of another\n");
+    printf("10. Small Straight (4 numbers are in sequence)\n");
+    printf("11. Large Straight (5 numbers are in sequence)\n");
+    printf("12. Yahtzee! (All 5 numbers are the same)\n");
+    printf("13. Chance (add up all numbers on dice) \n");
 
 
     }
@@ -59,19 +60,24 @@ void change_player(int *current_player){
 void start_game(){
     printf("Welcome to Yahtzee!\n");
 
+    
+
     int player1_combinations[] = {1,1,1,1,1,1,1,1,1,1,1,1,1};
     int player2_combinations[] = {1,1,1,1,1,1,1,1,1,1,1,1,1};
 
     int player1_score = 0, player2_score = 0;
     int player1_upperscore = 0, player2_upperscore = 0;
     int current_player = 1;
+    int *player_combinations, *score, *upper_score;
 
     int dice[5] = {1,2,3,4,5};
-    int dice_to_reroll[5];
+    // int dice_to_reroll[5];
     char y_n_reroll;
     int num_rolls = 0;
     int max_rolls = 3;
     int input_count = 0;
+
+
 
     for(int round = 0; round < 13; round++){
         system("clear"); // must be changed to 'cls' for windows
@@ -107,17 +113,17 @@ void start_game(){
         
 
         if (y_n_reroll == 'y'){
-            printf("Type the numbers of the dice you want to re-roll, separated by spaces. Enter -1 to indicate when you are done typing the dice you want to re-roll\n");
-
+            printf("Type the dice number you want to re-roll, followed by a space, a -1, and another space, then click enter. If you want to re-roll all 5 dice, do not click the -1 at the end, only click enter\n");
             input_count = 0;
 
             while (input_count < 5){
                 int index = 0;
                 if (scanf("%d", &index) == 1){
                     if (index == -1){
+                        
                         break;
                     }
-                    if (index >= 1 && index <= 6){
+                    if (index >= 1 && index <= 5){
                         dice[index - 1] = roll_dice();
                         input_count++;
                     
@@ -155,7 +161,6 @@ void start_game(){
 
 
        
-        int *player_combinations, *score, *upper_score;
        if (current_player == 1){
             player_combinations = player1_combinations;
             score = &player1_score;
@@ -166,19 +171,22 @@ void start_game(){
             score = &player2_score;
             upper_score = &player2_upperscore;
        }
+
     
 
         
-        printf("Please choose which game combination you would like to score in, refer to rules to what integer to type for each combination:\n");
+        printf("Please choose which game combination you would like to score in\n");
       
         
         while (getchar() != '\n');
         int comb_chosen = choose_combinations(player_combinations);
 
+        int sums = 0;
         switch (comb_chosen){
+            
             case 1: case 2: case 3: case 4: case 5: case 6:
-            sum_of_num(dice, score, comb_chosen);
-            *upper_score += sum_of_num(dice,score, comb_chosen);
+            sums = sum_of_num(dice, score, comb_chosen, player_combinations);
+            *upper_score += sums;
             break;
 
             case 7:
@@ -211,19 +219,37 @@ void start_game(){
 
         }
 
-
-        if (*upper_score >= 63){
+if (*upper_score >= 63){
             *score += 35;
-            printf("You got a 35 point bonus for have 63 or more points in your upper section!");
+            printf("You got a 35 point bonus for having 63 or more points in your upper section!");
         }
 
         printf("Player 1 Total: %d player 1 upper: %d\n", player1_score, player1_upperscore);
         printf("Player 2 Total: %d, player 2 upper: %d\n", player2_score, player2_upperscore);
 
-        system("pause");
+        printf("Press enter to continue...");
+        while (getchar() != '\n');
+        getchar();
         change_player(&current_player);
     }
+
+
+
+
+if (player1_score > player2_score){
+    printf("Player 1 wins!");
+}
+
+if (player1_score < player2_score){
+    printf("Player 2 wins!");
+}
+
+if (player1_score == player2_score){
+    printf("Its a tie!");
+}
     }
+
+   
 
     
 
@@ -251,7 +277,7 @@ int choose_combinations(int player_combinations[13]){
     printf("Available game combinations are below");
     for (int i=0; i <13; i++){
         if (player_combinations[i] == 1)
-            printf("%d. %s\n", i+1, combs[i]);
+            printf("\n%d. %s\n", i+1, combs[i]);
     }
 
     do {
@@ -273,10 +299,8 @@ int of_a_kind_checker(int dice[], int num_dice){
         counter[dice[i]]++;
     }
     for (int i = 1; i<= 6; i++){
-        int checked_number = -1;
         if (counter[i] >= num_dice){
-            checked_number = i;
-            return 1;
+            return i;
         }
     }
         
@@ -287,8 +311,9 @@ int of_a_kind_checker(int dice[], int num_dice){
 int three_of_a_kind(int dice[], int *score, int player_combinations[13]){
     if (player_combinations[6] == 1){
         int checker = of_a_kind_checker(dice, 3);
-        if (checker == 1)
+        if (checker != 0)
             *score += sum_dice(dice);
+            player_combinations[6] = 0;
         }
     return *score;
 }
@@ -299,12 +324,13 @@ int four_of_a_kind(int dice[], int *score, int player_combinations[13]){
     int checker = of_a_kind_checker(dice, 4);
     if (checker == 1)
         *score += sum_dice(dice);
+        player_combinations[7] = 0;
     }
     return *score;
 }
 
 
-int sum_of_num(int dice[], int *score, int num_to_sum){
+int sum_of_num(int dice[], int *score, int num_to_sum, int player_combinations[13]){
     int sum = 0;
     for (int i = 0; i < 5; i++){
         if (dice[i] == num_to_sum){
@@ -312,7 +338,8 @@ int sum_of_num(int dice[], int *score, int num_to_sum){
         }
     }
     *score += sum;
-    return *score;
+    player_combinations[num_to_sum - 1] = 0;
+    return sum; //change to score if doesnt work
 }
 
 
@@ -350,31 +377,26 @@ int small_straight(int dice[], int *score, int player_combinations[13]){
     int counter[7] = {0};
     int consec_nums = 0;
 
-    for (int i = 0; i < 5; i++){
-        if (counter[i]){
-            consec_nums++;
-            if (consec_nums >= 4){
-                *score += 30;
-                break;
-            }
-        }
-        else {
-            consec_nums = 0;
-        }
-    }
-
-    player_combinations[9] = 0;
-    return *score;
+for(int i = 0; i<5; i++){
+    counter[dice[i]]++;
 }
 
-
+for (int i = 1; i<= 3; i++){
+    if (counter[i] > 0 && counter[i+1] > 0 && counter [i+2]> 0 && counter [i+3] > 0){
+        *score += 30;
+        break;
+    }
+}
+player_combinations[9] = 0;
+return *score;
+}
 
 
 int large_straight (int dice[], int *score, int player_combinations[13]){
     int counter[7] = {0};
     
     for (int i = 0; i < 5; i++){
-        counter[dice[i]] = 1;
+        counter[dice[i]]++;
     }
     if (counter[1] == 1 && counter[2] == 1 && counter[3] == 1 && counter[4] == 1 && counter[5] == 1 ||
      counter[2] == 1 && counter[3] == 1 && counter[4] == 1 && counter[5] == 1 && counter[6] == 1){
@@ -396,9 +418,9 @@ int large_straight (int dice[], int *score, int player_combinations[13]){
 int yahtzee(int dice[], int *score, int player_combinations[13]){
     int counter[7] = {0};
     for (int i = 0; i < 5; i++){
-        counter[dice[i++]];
+        counter[dice[i]]++;
     }
-    for (int i = 0; i < 6; i++){
+    for (int i = 0; i <= 6; i++){
         if (counter[i] == 5){
             *score += 50;
             break;
@@ -406,6 +428,7 @@ int yahtzee(int dice[], int *score, int player_combinations[13]){
         }
 
     }
+    player_combinations[11] = 0;
     return *score;
     }
 
@@ -416,26 +439,6 @@ int chance (int dice[], int *score, int player_combinations[13]){
     player_combinations[12] = 0;
     return *score;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
